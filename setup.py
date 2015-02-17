534885704e1100ae3a268fea77acfaf17bda2c5e
James Pike <jpike@chilon.net>
2015-02-17 01:12:13 +0000

    0.0.3 - fix RPi.GPIO dependency.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='CatStalker',
-    version='0.0.2',
+    version='0.0.3',
     author='J. Pike',
     author_email='pip@chilon.net',
     scripts=['bin/catstalker.py'],
@@ -10,5 +10,5 @@ setup(
     license='LICENSE.txt',
     description='Control energenie radio control board.',
     long_description=open('README.txt').read(),
-    install_requires=["raspberry-gpio >= 0.5.0"],
+    install_requires=['RPi.GPIO >= 0.5.11'],
 )
